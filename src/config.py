b2e797b49cd92ca55a2a119ae8425e14401d2fac
unknown <karanjekarshardul3@gmail.com>
2026-04-26 23:32:59 +0530

    config updated

--- src/config.py
+++ src/config.py
@@ -8,7 +8,6 @@ PROJECT_ROOT = Path(__file__).parent.parent
 MODEL_DIR = PROJECT_ROOT / "MODEL"
 EMBED_MODEL_DIR = MODEL_DIR / "embeddings" / "bge-base-en-v1.5"
 LLM_MODEL_DIR = MODEL_DIR / "gemma-4-E4B-it-UD-Q4_K_XL.gguf"
-# MODEL\gemma-4-E4B-it-UD-Q4_K_XL.gguf  \MODEL\embeddings\bge-base-en-v1.5
 EVAL_DATA_DIR = PROJECT_ROOT / "data"
 DATA_DIR = PROJECT_ROOT / "data" / "TAX"
 VECTOR_DIR = PROJECT_ROOT / "vectorstore"
